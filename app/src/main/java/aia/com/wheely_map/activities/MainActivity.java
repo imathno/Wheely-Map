6c0ac1e9c91075b8052afaa679054d7e091ee769
Issa Mathno <imathno@hotmail.com>
2017-06-01 10:30:03 -0700

    Removed map activity and replaced it as a fragment (#10)
    * Added nav bar
    
    * Stuff is broken fix it
    
    * Updated so map is now a fragment
    Added navigation menu

--- app/src/main/java/aia/com/wheely_map/activities/MainActivity.java
+++ app/src/main/java/aia/com/wheely_map/activities/MainActivity.java
@@ -1,13 +1,57 @@
 package aia.com.wheely_map.activities;
 
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import aia.com.wheely_map.R;
+import aia.com.wheely_map.fragments.MapViewFragment;
+
+public class MainActivity extends FragmentActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        MapViewFragment fragment = new MapViewFragment();
+        fragmentTransaction.add(R.id.map_container, fragment, TAG);
+        fragmentTransaction.commit();
+    }
+
+    //broken
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        Intent activity;
+
+        if (id == R.id.nav_login) {
+            activity = new Intent(this, LoginActivity.class);
+        } else {
+            activity = null;
+        }
+
+        startActivity(activity);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/aia/com/wheely_map/fragments/MapViewFragment.java
@@ -0,0 +1,35 @@
+package aia.com.wheely_map.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import aia.com.wheely_map.R;
+
+public class MapViewFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                         Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+    }
+}
